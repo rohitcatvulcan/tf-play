a37098fc4720ae619072f26c11d8fdf591b3fff8
rohitc <rohitc@vulcan.com>
2019-10-29 05:49:33 -0700

    updated some values

--- example.tf
+++ example.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   profile    = "default"
-  region     = "us-east-1"
+  region     = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+resource "aws_instance" "tf-play" {
+  ami           = "ami-06d51e91cea0dac8d"
   instance_type = "t2.micro"
 }
